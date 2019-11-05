ab0cd1792691ec815c7112287b6a19ef31406d09
EAlexa <rubi_81@outlook.de>
2019-11-05 23:02:12 +0100

    Added TableView

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel!.text = "Hello World"
+        return cell
+    }
 }
-
