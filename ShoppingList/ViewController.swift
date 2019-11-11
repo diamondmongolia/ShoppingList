91a97512e714eea28249c11ef218a466337f90b7
EAlexa <rubi_81@outlook.de>
2019-11-11 12:55:17 +0100

    Added more functionality

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var itemCount: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
+    
+    
+}
+
 class ViewController: UIViewController, UITableViewDataSource {
+    
+    let itemName = ["Apfel", "Banane", "Orange"]
+    let itemCount = [4, 10, 3]
+    let itemImage = ["apfel.jpg", "banane.jpg", "orange.jpeg"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        //self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "custom")
+        
     }
 
     @IBOutlet weak var tableView: UITableView!
@@ -23,8 +38,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel!.text = "Hello World"
+        let cell : CustomTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "custom") as! CustomTableViewCell
+        
+        cell.itemName.text = itemName[indexPath.row]
+        cell.itemCount.text = "Count: \(itemCount[indexPath.row])"
+        cell.imageView?.image = UIImage(named:itemImage[indexPath.row])
+        
         return cell
     }
 }
